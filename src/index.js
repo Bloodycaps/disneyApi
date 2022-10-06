ee23942905ded9dab4dcc48cc87b4c3684154253
Danilo Cortes <dacortes63@misena.edu.co>
2022-10-05 19:32:17 -0500

    chore(style ): se da formato a los archivos de configuracion

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
+const { config } = require("./config/config");
 
 //Initializing
 const app = express();
 
-//Setting
-app.set('port', process.env.PORT || 3000);
+//Settings
+app.set("port", process.env.PORT || 3000);
 
 //Middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
 //Routes
-app.use(require('./routes/index.routes'));
+app.use(require("./routes/index.routes"));
+app.use("/genres", require("./routes/genres.routes"));
 
-app.listen(app.get('port'), () => {
-  console.log('servidor en el puerto ', app.get('port'));
+app.listen(config.port, () => {
+  console.log("server on port:", config.port);
 });
