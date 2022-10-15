c1502773545f15148d3202284d013e45f2986c6e
Danilo Cortes <dacortes63@misena.edu.co>
2022-10-15 13:08:03 -0500

    chore: se ajustan las rutas de patch y delete para la tabla genre

--- src/routes/genres.routes.js
+++ src/routes/genres.routes.js
@@ -10,18 +10,14 @@ const {
 const router = express.Router();
 const service = new GenreService();
 
-router.get(
-  "/",
-  validatorHandler(getGenreSchema, "params"),
-  async (req, res, next) => {
-    try {
-      const genres = await service.findAll();
-      res.json(genres);
-    } catch (error) {
-      next(error);
-    }
+router.get("/", async (req, res, next) => {
+  try {
+    const genres = await service.findAll();
+    res.json(genres);
+  } catch (error) {
+    next(error);
   }
-);
+});
 
 router.get(
   "/:id",
@@ -37,31 +33,35 @@ router.get(
   }
 );
 
-router.post("/", 
-validatorHandler(createGenreSchema, "body"),
-async (req, res, next) => {
-  try {
-    const body = req.body;
-    const newGenre = await service.create(body);
-    res.status(201).json(newGenre);
-  } catch (error) {
-    next(error);
+router.post(
+  "/",
+  validatorHandler(createGenreSchema, "body"),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newGenre = await service.create(body);
+      res.status(201).json(newGenre);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-router.patch("/:id",
-validatorHandler(getGenreSchema, "params"),
-validatorHandler(updateGenreSchema, "body"),
-async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const body = req.body;
-    const genre = await service.update(id, body);
-    res.json(genre);
-  } catch (error) {
-    next(error);
+router.patch(
+  "/:id",
+  validatorHandler(getGenreSchema, "params"),
+  validatorHandler(updateGenreSchema, "body"),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const genre = await service.update(id, body);
+      res.json(genre);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 router.delete("/:id", async (req, res, next) => {
   try {
